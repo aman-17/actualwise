15835a7bef7ef8a1ab7d96ed098f6127b5ac3629
aman-17 <amanrangapur@gmail.com>
2024-01-17 12:33:50 -0600

    updated ir

--- ocr/count_pages.py
+++ ocr/count_pages.py
@@ -1,5 +1,5 @@
 from pdf2image import convert_from_path
 
-images = convert_from_path('case1.pdf')
+images = convert_from_path('./CaseStudy_2_SD.pdf')
 for i, image in enumerate(images):
-    image.save(f'image_{i+1}.png', 'PNG')
+    image.save(f'./casestudy2/image_{i+1}.png', 'PNG')
--- ocr/ocr_tesseract.py
+++ ocr/ocr_tesseract.py
@@ -2,7 +2,8 @@ import fitz
 from io import BytesIO
 from PIL import Image
 import pytesseract
-
+import io
+import os
 # doc = fitz.open("case1.pdf")
 # page = doc.load_page(0)
 
@@ -18,13 +19,31 @@ import pytesseract
 # image.save(output_path)
 # image_io.close()
 
+# def ocr_core(image):
+#     text = pytesseract.image_to_string(Image.open(image))
+#     return text
+
+# def extract_text_from_image(image_file):
+#     text = ocr_core(image_file)
+#     return text
+
+# text = extract_text_from_image("image_1.png")
+# print(text)
+
+import os
+import pytesseract
+from PIL import Image
+
 def ocr_core(image):
     text = pytesseract.image_to_string(Image.open(image))
     return text
 
-def extract_text_from_image(image_file):
-    text = ocr_core(image_file)
-    return text
+def extract_text_from_folder(folder_path):
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".png") or filename.endswith(".jpg"):
+            image_path = os.path.join(folder_path, filename)
+            text = ocr_core(image_path)
+            print(f"Text extracted from {filename}:\n{text}\n{'-'*30}")
 
-text = extract_text_from_image("image_1.png")
-print(text)
+folder_path = './casestudy2'
+extract_text_from_folder(folder_path)

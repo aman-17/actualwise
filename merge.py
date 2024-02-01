4192e8e789bfa6e05670cb7b7116c61c6dc5a4b7
aman-17 <amanrangapur@gmail.com>
2024-02-01 17:22:51 -0600

    update

--- merge.py
+++ merge.py
@@ -1,19 +1,19 @@
 import os
 
-# directory = './deid/output/'
-# with open('merged.txt', 'w') as outfile:
-#     for filename in os.listdir(directory):
-#         if filename.endswith(".txt"):
-#             with open(os.path.join(directory, filename)) as infile:
-#                 next(infile)
-#                 for line in infile:
-#                     outfile.write(line)
+directory = './summary/'
+with open('merged2.txt', 'w') as outfile:
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):
+            with open(os.path.join(directory, filename)) as infile:
+                next(infile)
+                for line in infile:
+                    outfile.write(line)
 
 
-input_file = 'merged.txt'
-output_file = 'merged_new.txt'
+# input_file = 'merged.txt'
+# output_file = 'merged_new.txt'
 
-with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
-    for line in infile:
-        if line.strip():
-            outfile.write(line)
+# with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+#     for line in infile:
+#         if line.strip():
+#             outfile.write(line)

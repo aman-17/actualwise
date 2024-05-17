10bf40f126cae978f0454e862bc441b6a0a24a2d
aman-17 <amanrangapur@gmail.com>
2024-05-17 11:36:20 -0500

    addded chatRFP

--- chatRFP/ollama_api.py
+++ chatRFP/ollama_api.py
@@ -1,10 +1,15 @@
 import ollama
 
+input_file = 'chatRFP/ocr_outputs/RFP_2024_1145ActuarialServicesforOPEB 2.29.24_oem3_psm6_pytesseract_poppler_dpi600.txt'
+
+with open(input_file, 'r') as infile:
+    data = infile.read()
+# print(data)
+
 stream = ollama.chat(
-    model='phi3',
-    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+    model='llama3',
+    messages=[{'role': 'user', 'content': f'{data}. Based on the above data, answer these questions. What are insurance limits?  How many actives and retirees in the plan? What was last year\'s budget? '}],
     stream=True,
 )
-
 for chunk in stream:
   print(chunk['message']['content'], end='', flush=True)
